1c701653b85ab210185ce79a0d54d155401c5faa
Alexander Edwards <alexedwardsxyz@gmail.com>
2018-04-07 23:41:04 +1000

    2 Hours in - Table displaying results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 import { LoginComponent } from './components/login/login.component';
-import {AuthService} from "./services/auth.service";
+import { AuthService } from "./services/auth.service";
 import { MainComponent } from './components/main/main.component';
+import {ContactService} from "./services/contact.service";
 
 
 @NgModule({
@@ -23,13 +26,15 @@ import { MainComponent } from './components/main/main.component';
     RouterModule,
     AngularFireModule.initializeApp(environment.firebase, 'NetworkBook'),
     AngularFireAuthModule,
+    AngularFirestoreModule,
     RouterModule.forRoot([
       { path: '', component: MainComponent },
       { path: 'login', component: LoginComponent },
     ])
 ],
   providers: [
-    AuthService
+    AuthService,
+    ContactService,
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/contact.ts
@@ -0,0 +1,11 @@
+export interface Contact {
+  id?: string;
+  firstName?: string;
+  lastName?: string;
+  preferredName?: string;
+  email?: string;
+  phone?: string;
+
+  priority?: number;
+  lastContacted?: any;
+}
